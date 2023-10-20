9e02043439243d20e2522d012a6a4cadc7442e97
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-20 15:43:05 -0400

    Agregar archivos de proyecto.

---
+++ BookingSystem/Server/Models/Profesional.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingSystem.Server.Models
+{
+    public class Profesional
+    {
+        [Key]
+        public int ProfesionalId { get; set; }
+
+        [Required]
+        public string NombreCompleto { get; set; }
+
+        [Required]
+        public string Especialidad { get; set; }
+
+        public int ServicioId { get; set; }
+        [ForeignKey("ServicioId")]
+        public virtual Servicio? Servicio { get; set; }
+
+        public int HorarioId { get; set; }
+        [ForeignKey("HorarioId")]
+        public virtual Horario ?Horario { get; set; }
+
+
+    }
+}
