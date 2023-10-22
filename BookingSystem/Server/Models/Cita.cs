5cd3fc824c8aa0f4d9c6369b956dc9f207f88889
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-22 10:42:39 -0400

    Creating class Cita

---
+++ BookingSystem/Server/Models/Cita.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingSystem.Server.Models
+{
+    public class Cita
+    {
+        public int CitaId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Hora { get; set; }
+        public string Duracion { get; set; }
+        public bool Estado { get; set; }
+        
+        public int ProfesionalId { get; set;}
+        [ForeignKey("ProfesionalId")]
+        public virtual Profesional ?Profesional { get; set; }
+        public int ServicioId { get; set; }
+        [ForeignKey("ServicioId")]
+        public virtual Servicio ?Servicio { get; set; }
+    }
+}
