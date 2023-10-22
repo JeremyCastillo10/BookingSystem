4199b718fbd1ba2d443e4bd6b793ef29362a03fd
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-22 10:42:47 -0400

    Creating class Cliente

---
+++ BookingSystem/Server/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace BookingSystem.Server.Models
+{
+    public class Cliente
+    {
+        public string NombreCompleto { get; set; }
+        public string Email { get; set; }
+        public string Ciudad { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
