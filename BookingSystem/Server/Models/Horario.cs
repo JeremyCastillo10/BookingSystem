7e58af42d780cc136d5420ece59b773beb29b899
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-22 10:42:55 -0400

    Creating class Horario

--- BookingSystem/Server/Models/Horario.cs
+++ BookingSystem/Server/Models/Horario.cs
@@ -1,6 +1,13 @@
-﻿namespace BookingSystem.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingSystem.Server.Models
 {
     public class Horario
     {
+        [Key]
+        public int HorarioId { get; set; }
+        public string Dia { get; set; }
+        public string HoraInicio { get; set; }
+        public string HoraFin { get; set; }
     }
 }
