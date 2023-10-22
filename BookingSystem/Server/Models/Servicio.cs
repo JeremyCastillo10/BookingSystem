b9d4f89e3135ae3531be1fb07d279e235c3b1505
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-22 10:43:14 -0400

    Creating class Servicio

--- BookingSystem/Server/Models/Servicio.cs
+++ BookingSystem/Server/Models/Servicio.cs
@@ -1,6 +1,15 @@
-﻿namespace BookingSystem.Server.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingSystem.Server.Models
 {
     public class Servicio
     {
+        public int ServicioId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Duracion { get; set; }
+        public double Precio { get; set; }
+        public virtual Profesional ?Profesional { get; set;}
+        public virtual List<Profesional> Profesionales { get; set; } = new List<Profesional>();
     }
 }
