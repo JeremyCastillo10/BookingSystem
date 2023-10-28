4d7e401b0abe5ef81a75b7d91d165abefeaa1b55
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-28 19:11:11 -0400

    profesionalController with post get and getid

---
+++ BookingSystem/Server/Controllers/ProfesionalController.cs
@@ -0,0 +1,54 @@
+﻿using BookingSystem.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingSystem.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfesionalController : ControllerBase
+    {
+        private readonly Contexto _contexto;
+        public ProfesionalController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Profesional>>> GetProfesional()
+        {
+            var profesionales = await _contexto.Profesional.ToListAsync();
+            return Ok(profesionales);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Profesional> GetProfesionalPorId(int id)
+        {
+            var profesional = _contexto.Profesional.FirstOrDefault(p => p.ProfesionalId == id);
+            return Ok(profesional);
+        }
+        [HttpPost]
+        [Route("Guardar")]
+        public async Task<ActionResult> PostProfesional(Profesional profesional)
+        {
+            _contexto.Add(profesional);
+            await _contexto.SaveChangesAsync();
+            return Ok(profesional);
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteProfesional(int id)
+        {
+            var profesional = await _contexto.Profesional.FindAsync(id);
+            if (profesional == null)
+            {
+                return NotFound();
+            }
+            profesional.Visible = false;
+            await _contexto.SaveChangesAsync();
+            return Ok(profesional);
+            
+
+        }
+    }
+}
