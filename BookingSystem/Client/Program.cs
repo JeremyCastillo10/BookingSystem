19bbf4a7f18d885ba93c980439968e916dfcd37b
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-10-28 21:44:14 -0400

    adding mudBlazor

--- BookingSystem/Client/Program.cs
+++ BookingSystem/Client/Program.cs
@@ -4,26 +4,18 @@ global using System.Net.Http.Json;
 using BookingSystem.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MatBlazor;
+using MudBlazor.Services;
 
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddMatToaster(config =>
-{
-    config.Position = MatToastPosition.BottomRight;
-    config.PreventDuplicates = true;
-    config.NewestOnTop = true;
-    config.ShowCloseButton = true;
-    config.MaximumOpacity = 95;
-    config.VisibleStateDuration = 3000;
-});
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IHorarioRepositorio, HorarioRepositorio>();
 builder.Services.AddScoped<IProfesionalRepositorio, ProfesionalRepositorio>();
+builder.Services.AddMudServices();
 
 
 await builder.Build().RunAsync();
