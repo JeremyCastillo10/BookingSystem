a5328f432ef973cd4597608f867f4648123587d0
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-11-02 13:54:39 -0400

    validation with sweetalert2

--- BookingSystem/Client/Program.cs
+++ BookingSystem/Client/Program.cs
@@ -2,9 +2,11 @@ global using BookingSystem.Client.Repositorios;
 global using BookingSystem.Shared;
 global using System.Net.Http.Json;
 using BookingSystem.Client;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+
 
 
 
@@ -17,6 +19,8 @@ builder.Services.AddScoped<IServicioRepositorio, ServicioRepositorio>();
 builder.Services.AddScoped<IProfesionalRepositorio, ProfesionalRepositorio>();
 builder.Services.AddScoped<IProfesionalDetalleRepositorio, ProfesionalDetalleRepositorio>();
 builder.Services.AddMudServices();
+builder.Services.AddSweetAlert2();
+
 
 
 await builder.Build().RunAsync();
