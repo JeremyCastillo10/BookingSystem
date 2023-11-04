b412f0c37ec0e80fbe5baa270d69f25b1e2c8b84
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-11-03 21:06:39 -0400

    Modal

--- BookingSystem/Client/Program.cs
+++ BookingSystem/Client/Program.cs
@@ -1,6 +1,9 @@
 global using BookingSystem.Client.Repositorios;
 global using BookingSystem.Shared;
 global using System.Net.Http.Json;
+using Blazorise;
+using Blazorise.Icons.Material;
+using Blazorise.Material;
 using BookingSystem.Client;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
@@ -20,6 +23,13 @@ builder.Services.AddScoped<IProfesionalRepositorio, ProfesionalRepositorio>();
 builder.Services.AddScoped<IProfesionalDetalleRepositorio, ProfesionalDetalleRepositorio>();
 builder.Services.AddMudServices();
 builder.Services.AddSweetAlert2();
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddMaterialProviders()
+    .AddMaterialIcons();
 
 
 
